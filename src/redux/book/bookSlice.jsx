4b846f27575a2494149f34409f2e0f4eaba9d5a3
JimOluwaseyi <heysey128@gmail.com>
2024-01-19 12:23:19 +0100

    Add the global array for the app

--- src/redux/book/bookSlice.jsx
+++ src/redux/book/bookSlice.jsx
@@ -1,7 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  book: [],
+  books:  [
+    {
+      "item_id": "item1",
+      "title": "The Great Gatsby",
+      "author": "John Smith",
+      "category": "Fiction"
+    },
+    {
+      "item_id": "item2",
+      "title": "Anna Karenina",
+      "author": "Leo Tolstoy",
+      "category": "Fiction"
+    },
+    {
+      "item_id": "item3",
+      "title": "The Selfish Gene",
+      "author": "Richard Dawkins",
+      "category": "Nonfiction"
+    }
+  ]
 };
 
 const bookSlice = createSlice({
@@ -9,10 +28,10 @@ const bookSlice = createSlice({
   initialState,
   reducers: {
     addBook:(state,action)=>{
-state.book.push(action.payload)
-    }, 
-    removeBook: (state , action)=>{
-state.book= state.book.filter((item,idx)=> idx !== action.payload)
+      state.books.push(action.payload);
+    },
+    removeBook:(state,action)=>{
+      state.books = state.books.filter((book)=> book.item_id !== action.payload)
     }
   },
 });
